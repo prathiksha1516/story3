93bf58b7db91477d62e556487fa4010cb0c0f5e3
prathiksha1516 <71827736+prathiksha1516@users.noreply.github.com>
2021-04-21 11:53:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-p7N1k1BuKKla0VM_BQdDlchTmgoe5mI",
+    authDomain: "story-hub-a7db0.firebaseapp.com",
+    projectId: "story-hub-a7db0",
+    storageBucket: "story-hub-a7db0.appspot.com",
+    messagingSenderId: "209762387452",
+    appId: "1:209762387452:web:74ef5bd1940880d78b5e79"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
